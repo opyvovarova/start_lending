bf5dc11c0959381d4ef2bee3b17c00f8b592aec2
AKD47 <van83horn@gmail.com>
2016-10-04 15:43:05 +0300

    top-slider start

--- app/js/script.js
+++ app/js/script.js
@@ -1,4 +1,29 @@
 $(document).ready(function(){
 
-   
+    /*top-slider*/
+    var top = $('.top-slider__box');
+
+    top.owlCarousel({
+        animateOut: 'fadeOut',
+        animateIn: 'fadeIn',
+        loop: true,
+        margin: 0,
+        stagePadding: 0,
+        smartSpeed: 450,
+        nav: false,
+        autoplay: true,
+        responsive: {
+            0: {
+                items: 1
+            },
+            600: {
+                items: 1
+            },
+            1000: {
+                items: 1
+            }
+        }
+    });
+    /*close top-slider*/
+    
 });
