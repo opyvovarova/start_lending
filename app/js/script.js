bf5a010da9a39f6319b6fadf782f82222c3840a7
AKD47 <van83horn@gmail.com>
2016-10-05 14:57:45 +0300

    imgs

--- app/js/script.js
+++ app/js/script.js
@@ -1,4 +1,4 @@
-$(document).ready(function(){
+$(document).ready(function () {
 
     /*menu scroll*/
     $('.smoothScroll').click(function (event) {
@@ -11,7 +11,7 @@ $(document).ready(function(){
     });
     /*close menu scroll*/
 
-    /*top-slider*/   
+    /*top-slider*/
     $('.top-slider__box').owlCarousel({
         animateOut: 'flipOutX',
         animateIn: 'flipInX',
@@ -34,5 +34,25 @@ $(document).ready(function(){
         }
     });
     /*close top-slider*/
-    
+
+    /*go up arrow*/
+    $(window).scroll(function () {
+        var scrollDiv = $('#Go_Top');
+        $(this).hide().removeAttr("href");
+        if ($(window).scrollTop() >= "250") {/*показывать при прокрутке вниз показывать медленно #Go_Top*/
+            $(this).fadeIn("slow")
+        };
+        if ($(window).scrollTop() <= "250") {/*при прокрутке ввверх медленно скрывать #Go_Top*/
+            $(scrollDiv).fadeOut("slow");
+        }
+        else {
+            $(scrollDiv).fadeIn("slow")
+        };
+    });
+    $('#Go_Top').click(function () {/*функция плавной прокрутки вверх при клике на стрелку "вверх"*/
+        $("html, body").animate({scrollTop: 0}, "slow");
+        return false;
+    });
+    /*----- close-----*/
+
 });
