7e82231f326176d396ebb2cc293746be1cca5868
AKD47 <van83horn@gmail.com>
2016-10-10 10:13:17 +0300

    fix

--- framework-customizations/extensions/shortcodes/shortcodes/brand/views/view.php
+++ framework-customizations/extensions/shortcodes/shortcodes/brand/views/view.php
@@ -1,29 +1,31 @@
-<?php if ( ! defined( 'FW' ) ) {
-    die( 'Forbidden' );
+<?php if (!defined('FW')) {
+    die('Forbidden');
 } ?>
 
 <!-- start brand.html-->
 <section class="brand">
-	<div class="container">
-		<a name="brand" id="go_brand"></a>
+    <div class="container">
+        <a name="brand" id="go_brand"></a>
 		<span class="brand__img">
             <img src="<?php bloginfo('template_directory'); ?>/img/lightning.png" alt="">
         </span>
-		<p class="brand__uptitle"><?php echo $atts['subtitle']; ?></p>
-		<h3 class="brand__title"><?php echo $atts['title']; ?></h3>
-		<div class="brand__box">
-			<div class="brand__box--left">
+        <p class="brand__uptitle"><?php echo $atts['subtitle']; ?></p>
+        <h3 class="brand__title"><?php echo $atts['title']; ?></h3>
+        <div class="brand__box">
+            <div class="brand__box--left">
                 <span class="brand__box--img">
-	                <?php if(isset($atts['image']['url'])): ?>
-		                <img src="<?php echo $atts['image']['url']; ?>" alt="">
-	                <?php endif; ?>
+	                <?php if (isset($atts['image']['url'])): ?>
+                        <img src="<?php echo $atts['image']['url']; ?>" alt="">
+                    <?php endif; ?>
                 </span>
-			</div>
-			<div class="brand__box--right">
-				<?php echo $atts['text']; ?>
-				<a href="#" class="brand__box--order">Заказать продукцию</a>
-			</div>
-		</div>
-	</div>
+            </div>
+            <div class="brand__box--right">
+                <div class="brand__box--container">
+                    <?php echo $atts['text']; ?>
+                </div>
+                <a href="#" class="brand__box--order">Заказать продукцию</a>
+            </div>
+        </div>
+    </div>
 </section>
 <!-- end brand.html-->
